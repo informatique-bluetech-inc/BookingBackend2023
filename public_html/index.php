e1bd1646e176b423ed0b76e4fc62157754ff7e33
bluetchca <138949146+bluetchca@users.noreply.github.com>
2023-09-17 00:34:13 -0400

    Update App

--- public_html/index.php
+++ public_html/index.php
@@ -15,12 +15,3 @@ if(count(array_filter($arrayRutas)) == 2){
      }
 }
 
-if(isset($_SERVER["REQUEST_METHOD"]) && $_SERVER["REQUEST_METHOD"] == "GET"){
-        header('Content-Type: application/json; charset=utf-8');
-        $response = [
-            "status" => 404,
-            "msg" => "No Found...",
-        ];
-
-        echo json_encode($response);
- }
