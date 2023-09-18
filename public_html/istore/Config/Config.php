e3826a234d85ea10229b9433a2bc6f288a51857f
bluetchca <138949146+bluetchca@users.noreply.github.com>
2023-09-17 23:05:29 -0400

    Update Key Store

--- public_html/istore/Config/Config.php
+++ public_html/istore/Config/Config.php
@@ -7,15 +7,15 @@ class Config
     static public function data()
     {
         return [
-            "REST_CERT_PATH" => dirname(__FILE__, 1) . "/AppleCare-Partner-0001259855.Prod.apple.com.cert.pem",
+            "REST_CERT_PATH" => dirname(__FILE__, 1) . "/AppleCare-Partner-0001054076.Prod.apple.com.chain.pem",
             "REST_SSL_KEY" => dirname(__FILE__, 1) . "/privatekey.pem",
-            "REST_CERT_PASS" => "BluetechAppleGSX2022",
-            "REST_ACCOUNT_ID" => "gsxapi@ibluetech.ca",
+            "REST_CERT_PASS" => "iStore07631Englewood",
+            "REST_ACCOUNT_ID" => "mudit@ishopsms.com",
             "REST_BASE_URL" => "https://api-partner-connect.apple.com",
             "REST_AUTH_TOKEN_APPLE" => "c63afca2-8fd6-4a73-880a-74497e945e1g",
             "REST_AUTH_TOKEN" => "",
-            "REST_SoldTo" => "0001259855",
-            "REST_ShipTo" => "0001259855",
+            "REST_SoldTo" => "0001054076",
+            "REST_ShipTo" => "0001054076",
             "REST_AUTH_PATH" => "/api",
             "REST_GSX_PATH" => "/gsx/api",
         ];
