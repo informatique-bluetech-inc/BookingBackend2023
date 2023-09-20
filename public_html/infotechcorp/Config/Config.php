fa25e00fbbbef350637ada3c3d0174f663aa629d
unknown <sergiogasparllanos11@gmail.com>
2023-09-19 22:07:45 -0500

    add pass certificate

--- public_html/infotechcorp/Config/Config.php
+++ public_html/infotechcorp/Config/Config.php
@@ -9,7 +9,7 @@ class Config
         return [
             "REST_CERT_PATH" => dirname(__FILE__, 1) . "/AppleCare-Partner-0000023532.Prod.apple.com.chain.pem",//change
             "REST_SSL_KEY" => dirname(__FILE__, 1) . "/privatekey.pem",//change
-            "REST_CERT_PASS" => "VE?X*0Crkd7>sfPF@@S.",//change
+            "REST_CERT_PASS" => "Subject-Gawk7",//change
             "REST_ACCOUNT_ID" => "bluetech-apple@infotechcorporation.com",//change
             "REST_BASE_URL" => "https://api-partner-connect.apple.com",
             "REST_AUTH_TOKEN_APPLE" => "42d562c0-b7d5-49fc-9173-77a36f64d68a",//change
