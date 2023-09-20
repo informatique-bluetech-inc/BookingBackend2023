cccf0132b63aaa6a9c3887ce1ccd81dd53daca62
bluetchca <138949146+bluetchca@users.noreply.github.com>
2023-09-19 21:07:24 -0400

    Update Backend

--- public_html/istore_two/index.php
+++ public_html/istore_two/index.php
@@ -1,9 +1,4 @@
 <?php
-date_default_timezone_set('Canada/Central');
 
-require_once __DIR__."/Controllers/RutasController.php";
-
-$router = new controllers\RutasiStoreTwoController();
-$router->index();
 
 
