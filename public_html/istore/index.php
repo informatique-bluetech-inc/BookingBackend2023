b5b09894336a5c6e23251fed069686d58bba74d5
bluetchca <138949146+bluetchca@users.noreply.github.com>
2023-09-19 20:36:06 -0400

    Update Backen

--- public_html/istore/index.php
+++ public_html/istore/index.php
@@ -1,9 +1,1 @@
 <?php
-date_default_timezone_set('Canada/Central');
-
-require_once __DIR__."/Controllers/RutasController.php";
-
-$router = new controllers\RutasiStoreController();
-$router->index();
-
-
