c1c36aa6f97ae06e0efa2665fa108e46832a2e73
unknown <sergiogasparllanos11@gmail.com>
2023-09-19 21:55:48 -0500

    upload keys and certificates

--- public_html/infotechcorp/Config/Config.php
+++ public_html/infotechcorp/Config/Config.php
@@ -7,18 +7,18 @@ class Config
     static public function data()
     {
         return [
-            "REST_CERT_PATH" => dirname(__FILE__, 1) . "/AppleCare-Partner-0001259855.Prod.apple.com.cert.pem",
-            "REST_SSL_KEY" => dirname(__FILE__, 1) . "/privatekey.pem",
-            "REST_CERT_PASS" => "VE?X*0Crkd7>sfPF@@S.",
-            "REST_ACCOUNT_ID" => "bluetech-apple@infotechcorporation.com",
+            "REST_CERT_PATH" => dirname(__FILE__, 1) . "/AppleCare-Partner-0000023532.Prod.apple.com.chain.pem",//change
+            "REST_SSL_KEY" => dirname(__FILE__, 1) . "/privatekey.pem",//change
+            "REST_CERT_PASS" => "VE?X*0Crkd7>sfPF@@S.",//change
+            "REST_ACCOUNT_ID" => "bluetech-apple@infotechcorporation.com",//change
             "REST_BASE_URL" => "https://api-partner-connect.apple.com",
-            "REST_AUTH_TOKEN_APPLE" => "42d562c0-b7d5-49fc-9173-77a36f64d68a",
+            "REST_AUTH_TOKEN_APPLE" => "42d562c0-b7d5-49fc-9173-77a36f64d68a",//change
             "REST_AUTH_TOKEN" => "",
-            "REST_SoldTo" => "0000023532",
-            "REST_ShipTo" => "0000023532",
+            "REST_SoldTo" => "0000023532",//change
+            "REST_ShipTo" => "0000023532",//change
             "REST_AUTH_PATH" => "/api",
             "REST_GSX_PATH" => "/gsx/api",
-            "REST_TABLE" => "infotechcorp"
+            "REST_TABLE" => "infotechcorp"//change
         ];
     }
 
