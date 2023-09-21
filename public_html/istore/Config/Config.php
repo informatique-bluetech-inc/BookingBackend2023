a56607d0276666efc4acddd2a0be07f2a8d7ebc7
Sergio Gaspar <sergiogasparllanos11@gmail.com>
2023-09-20 21:22:59 -0500

    sube token apple

--- public_html/istore/Config/Config.php
+++ public_html/istore/Config/Config.php
@@ -12,7 +12,7 @@ class Config
             "REST_CERT_PASS" => "iStore07631Englewood",
             "REST_ACCOUNT_ID" => "mudit@ishopsms.com",
             "REST_BASE_URL" => "https://api-partner-connect.apple.com",
-            "REST_AUTH_TOKEN_APPLE" => "40606cb5-1374-4ad8-9b81-c0d76c7c206y",
+            "REST_AUTH_TOKEN_APPLE" => "dbe4c917-66fe-45de-b153-8335c22f84fx",
             "REST_AUTH_TOKEN" => "",
             "REST_SoldTo" => "0001054076",
             "REST_ShipTo" => "0001054076",//35 S Van Brunt St, Englewood
