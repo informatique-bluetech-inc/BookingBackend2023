39ebe2ee80a5fb7e2412cc6c67353ceeba8ef87d
Sergio Gaspar <sergiogasparllanos11@gmail.com>
2023-09-20 21:51:11 -0500

    change token in 3 istore

--- public_html/istore/Config/Config.php
+++ public_html/istore/Config/Config.php
@@ -12,7 +12,7 @@ class Config
             "REST_CERT_PASS" => "iStore07631Englewood",
             "REST_ACCOUNT_ID" => "mudit@ishopsms.com",
             "REST_BASE_URL" => "https://api-partner-connect.apple.com",
-            "REST_AUTH_TOKEN_APPLE" => "dbe4c917-66fe-45de-b153-8335c22f84fx",
+            "REST_AUTH_TOKEN_APPLE" => "f90a7ef2-6979-44e6-a676-43e3a490049u",
             "REST_AUTH_TOKEN" => "",
             "REST_SoldTo" => "0001054076",
             "REST_ShipTo" => "0001054076",//35 S Van Brunt St, Englewood
