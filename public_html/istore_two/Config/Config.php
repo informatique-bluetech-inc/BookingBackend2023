2d2274cf5bfd51646113026b22aeedbe1371a616
Sergio Gaspar <sergiogasparllanos11@gmail.com>
2023-09-20 21:47:08 -0500

    add configs for istore two and three

--- public_html/istore_two/Config/Config.php
+++ public_html/istore_two/Config/Config.php
@@ -7,18 +7,18 @@ class Config
     static public function data()
     {
         return [
-            "REST_CERT_PATH" => dirname(__FILE__, 1) . "/AppleCare-Partner-0001259855.Prod.apple.com.cert.pem",
+            "REST_CERT_PATH" => dirname(__FILE__, 1) . "/AppleCare-Partner-0001054076.Prod.apple.com.chain.pem",
             "REST_SSL_KEY" => dirname(__FILE__, 1) . "/privatekey.pem",
-            "REST_CERT_PASS" => "BluetechAppleGSX2022",
-            "REST_ACCOUNT_ID" => "gsxapi@ibluetech.ca",
+            "REST_CERT_PASS" => "iStore07631Englewood",
+            "REST_ACCOUNT_ID" => "mudit@ishopsms.com",
             "REST_BASE_URL" => "https://api-partner-connect.apple.com",
-            "REST_AUTH_TOKEN_APPLE" => "c63afca2-8fd6-4a73-880a-74497e945e1g",
+            "REST_AUTH_TOKEN_APPLE" => "dbe4c917-66fe-45de-b153-8335c22f84fx",
             "REST_AUTH_TOKEN" => "",
-            "REST_SoldTo" => "0001259855",
-            "REST_ShipTo" => "0001259855",
+            "REST_SoldTo" => "0001054076",
+            "REST_ShipTo" => "0001103607",//36W 36th St, Floor 4, New York
             "REST_AUTH_PATH" => "/api",
             "REST_GSX_PATH" => "/gsx/api",
-            "REST_TABLE" => "istore"
+            "REST_TABLE" => "istore",
         ];
     }
 
