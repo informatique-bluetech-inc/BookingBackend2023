4a619d5bdf44d58ba879c355a7ecbf1fb9ba2ea1
bluetchca <138949146+bluetchca@users.noreply.github.com>
2023-09-22 20:31:47 -0400

    Update Backend

--- public_html/istore/Rutas/Rutas.php
+++ public_html/istore/Rutas/Rutas.php
@@ -1,17 +1,8 @@
 <?php
-date_default_timezone_set("America/New_York");
 require_once __DIR__."/../Controllers/AuthController.php";
 require_once __DIR__."/../Controllers/ReservationController.php";
 
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
-    header('Access-Control-Allow-Headers: token, Content-Type');
-    header('Access-Control-Max-Age: 1728000');
-    header('Content-Length: 0');
-    header('Content-Type: text/plain');
-    die();
-}
+date_default_timezone_set("America/New_York");
 
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
