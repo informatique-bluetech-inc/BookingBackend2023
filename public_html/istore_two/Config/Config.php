9a58366d195c3acace99d66ededa8e567ef64573
bluetchca <138949146+bluetchca@users.noreply.github.com>
2023-09-25 19:55:45 -0400

    Update Backend

--- public_html/istore_two/Config/Config.php
+++ public_html/istore_two/Config/Config.php
@@ -12,7 +12,7 @@ class Config
             "REST_CERT_PASS" => "iStore07631Englewood",
             "REST_ACCOUNT_ID" => "mudit@ishopsms.com",
             "REST_BASE_URL" => "https://api-partner-connect.apple.com",
-            "REST_AUTH_TOKEN_APPLE" => "258c30f3-967a-46de-a9bc-d0975ee6c44p",
+            "REST_AUTH_TOKEN_APPLE" => "67392221-6fed-46fa-b993-044858af0b2y",
             "REST_AUTH_TOKEN" => "",
             "REST_SoldTo" => "0001103607",
             "REST_ShipTo" => "0001103607",
