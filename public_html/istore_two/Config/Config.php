5300ac0eb944df68cd5b60367a9b9ecd79a1873e
bluetchca <138949146+bluetchca@users.noreply.github.com>
2023-09-25 19:51:47 -0400

    Update Backend

--- public_html/istore_two/Config/Config.php
+++ public_html/istore_two/Config/Config.php
@@ -10,7 +10,7 @@ class Config
             "REST_CERT_PATH" => dirname(__FILE__, 1) . "/AppleCare-Partner-0001054076.Prod.apple.com.chain.pem",
             "REST_SSL_KEY" => dirname(__FILE__, 1) . "/privatekey.pem",
             "REST_CERT_PASS" => "iStore07631Englewood",
-            "REST_ACCOUNT_ID" => "service@istorestm.com",
+            "REST_ACCOUNT_ID" => "mudit@ishopsms.com",
             "REST_BASE_URL" => "https://api-partner-connect.apple.com",
             "REST_AUTH_TOKEN_APPLE" => "323c9ccb-5fb7-4e9e-8106-63d77a2fffdx",
             "REST_AUTH_TOKEN" => "",
