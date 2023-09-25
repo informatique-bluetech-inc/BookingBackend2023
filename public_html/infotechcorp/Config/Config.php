1d333202973e10e1babdd446319bc2a71929d6d8
bluetchca <138949146+bluetchca@users.noreply.github.com>
2023-09-25 18:15:10 -0400

    Update Backend

--- public_html/infotechcorp/Config/Config.php
+++ public_html/infotechcorp/Config/Config.php
@@ -10,9 +10,9 @@ class Config
             "REST_CERT_PATH" => dirname(__FILE__, 1) . "/AppleCare-Partner-0000023532.Prod.apple.com.chain.pem",//change
             "REST_SSL_KEY" => dirname(__FILE__, 1) . "/privatekey.pem",//change
             "REST_CERT_PASS" => "Subject-Gawk7",//change
-            "REST_ACCOUNT_ID" => "Mrameez-apple@infotechcorporation.com",//change
+            "REST_ACCOUNT_ID" => "bluetech-apple@infotechcorporation.com",//change
             "REST_BASE_URL" => "https://api-partner-connect.apple.com",
-            "REST_AUTH_TOKEN_APPLE" => "25f9613d-e93c-4c70-b75f-cb40b09ee09m",//change
+            "REST_AUTH_TOKEN_APPLE" => "c82869cb-bec6-4a40-8a0d-92cd9336642j",//change
             "REST_AUTH_TOKEN" => "",
             "REST_SoldTo" => "0000023532",//change
             "REST_ShipTo" => "0000023532",//change
