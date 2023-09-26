f358bf754f502bdde8aacb30bb19e2e2bc66d9b5
bluetchca <138949146+bluetchca@users.noreply.github.com>
2023-09-25 20:53:49 -0400

    Update Backend

--- public_html/bluetech/Config/Config.php
+++ public_html/bluetech/Config/Config.php
@@ -12,7 +12,7 @@ class ConfigBluetech
             "REST_CERT_PASS" => "BluetechAppleGSX2022",
             "REST_ACCOUNT_ID" => "gsxapi@ibluetech.ca",
             "REST_BASE_URL" => "https://api-partner-connect.apple.com",
-            "REST_AUTH_TOKEN_APPLE" => "c63afca2-8fd6-4a73-880a-74497e945e1g",
+            "REST_AUTH_TOKEN_APPLE" => "bd27fde4-a58d-41b5-96fb-517b80b384aa",
             "REST_AUTH_TOKEN" => "",
             "REST_SoldTo" => "0001259855",
             "REST_ShipTo" => "0001259855",
