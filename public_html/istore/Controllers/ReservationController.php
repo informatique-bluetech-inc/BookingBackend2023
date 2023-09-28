8e158f3a2b21d87027ba7284aebb80e7f487eb09
bluetchca <138949146+bluetchca@users.noreply.github.com>
2023-09-27 22:52:16 -0400

    Update Backend

--- public_html/istore/Controllers/ReservationController.php
+++ public_html/istore/Controllers/ReservationController.php
@@ -63,7 +63,7 @@ class ReservationController
         foreach ($slots[2]->slots as $item) {
             if (date('Y-m-d', strtotime($item->end . " UTC")) == $filter_date) {
                 $time = date('H:i', strtotime($item->start.'+1 hour'));
-                if(in_array($hours_available, [$time])){
+                if(in_array($time, $hours_available)){
                     $hours_available[] = $time;
                 }
                
