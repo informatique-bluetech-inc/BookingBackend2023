b26e3edf20369681710a08419c33cf20be6dd8c5
SergioGasparLlanos <sergiogasparllanos11@gmail.com>
2023-10-01 00:00:16 -0500

    Update Config.php

--- public_html/infotechcorp/Config/Config.php
+++ public_html/infotechcorp/Config/Config.php
@@ -12,7 +12,7 @@ class Config
             "REST_CERT_PASS" => "Subject-Gawk7",//change
             "REST_ACCOUNT_ID" => "bluetech-apple@infotechcorporation.com",//change
             "REST_BASE_URL" => "https://api-partner-connect.apple.com",
-            "REST_AUTH_TOKEN_APPLE" => "5b3f88df-6965-446c-ad08-3cba8eeb377d",//change
+            "REST_AUTH_TOKEN_APPLE" => "3660f59f-76ad-47db-aee4-a27645b29bcc",//change
             "REST_AUTH_TOKEN" => "",
             "REST_SoldTo" => "0000023532",//change
             "REST_ShipTo" => "0000023532",//change
