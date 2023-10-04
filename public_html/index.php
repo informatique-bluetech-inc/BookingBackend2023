63e3da83bf53c4cc21cb2eb7df014bc6d0473cc9
SergioGasparLlanos <sergiogasparllanos11@gmail.com>
2023-10-03 19:15:36 -0500

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -15,11 +15,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
   header('Content-Length: 0');
   header('Content-Type: text/plain');
   die();
-}
+}*/
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
 header('Content-Type: application/json');
-*/
+
 
 
 
