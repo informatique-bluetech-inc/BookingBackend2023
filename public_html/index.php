ce95e48ccb87e8380ddc1c43d27c82c88293af57
SergioGasparLlanos <sergiogasparllanos11@gmail.com>
2023-10-05 14:26:15 -0500

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -12,31 +12,26 @@ $INFOTECHCORP = "infotechcorp";
 if(count(array_filter($arrayRutas)) == 2){
 
     if(array_filter($arrayRutas)[1] == "bluetech"){
-        echo "bluetech";
         $router = new RutasController();
         $router->index($BLUETECH);
     }
     
     if(array_filter($arrayRutas)[1] == "istore"){
-        echo "istore";
         $router = new RutasController();
         $router->index($ISTORE1);
     }
     
     if(array_filter($arrayRutas)[1] == "istore_two"){
-        echo "istore_two";
         $router = new RutasController();
         $router->index($ISTORE2);
     }
 
     if(array_filter($arrayRutas)[1] == "istore_three"){
-        echo "istore_three";
         $router = new RutasController();
         $router->index($ISTORE3);
     }
     
     if(array_filter($arrayRutas)[1] == "infotechcorp"){
-        echo "infotechcorp";
         $router = new RutasController();
         $router->index($INFOTECHCORP);
     }
