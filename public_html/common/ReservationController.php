04a7c92542844e339ad1fe95498c26781c10a4be
SergioGasparLlanos <sergiogasparllanos11@gmail.com>
2023-10-05 23:38:23 -0500

    Update ReservationController.php

--- public_html/common/ReservationController.php
+++ public_html/common/ReservationController.php
@@ -151,6 +151,8 @@ class ReservationController{
         curl_setopt($ch, CURLOPT_SSLKEY, $storeInfo["REST_SSL_KEY"]);
         curl_setopt($ch, CURLOPT_SSLCERTPASSWD, $storeInfo["REST_CERT_PASS"]);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $requestHeaders);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30); 
+        curl_setopt($ch, CURLOPT_TIMEOUT, 40); //timeout in seconds
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
         $result = curl_exec($ch);
